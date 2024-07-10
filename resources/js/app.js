78285a490d9e2503ae172812711c74296878f6f7
sota <mtnsot919@icloud.com>
2024-07-10 14:39:49 +0000

    更新

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,40 @@ if(lb != null){
         p.classList.add('active');
     });
 }
-const bc = document.querySelector('.btn-close');
+
+const eb = document.getElementById('eb');
+const pE = document.querySelector('.popupEmail');
+if(eb != null){
+    eb.addEventListener('click', function() {
+        pE.classList.add('active');
+    });
+}
+
+const pb = document.getElementById('pb');
+const pP = document.querySelector('.popupPassword');
+if(pb != null){
+    pb.addEventListener('click', function() {
+        pP.classList.add('active');
+    });
+}
+
+const bc = document.querySelector('.btn-close-logout');
 
     bc.addEventListener('click', function() {
         p.classList.remove('active');
     });
 
+const bce = document.querySelector('.btn-close-email');
+
+    bce.addEventListener('click', function() {
+        pE.classList.remove('active');
+    });
+
+const bcp = document.querySelector('.btn-close-password');
+
+    bcp.addEventListener('click', function() {
+        pP.classList.remove('active');
+    });
 
 const btn = document.getElementById('logout');     //同じページにidがない場合jsがえらーをおこすのでif文で分岐させる
 const box = document.querySelector('.logout-box');
--- resources/views/admin/gamingposts/settings.blade.php
+++ resources/views/admin/gamingposts/settings.blade.php
@@ -10,37 +10,39 @@
     <ul class="settingMenu">
         <li>
             <div><img src="{{ secure_asset('') }}" alt="画像"></div>
-            <p>アカウント</p>
+            <p class="text-center">アカウント</p>
         </li>
         <li>
-            <a id="lb"><div>ログアウト</div></a>
+            <a id="lb" class="settingMenuButton">ログアウト</a>
             <div class="popup">
-                <h1>ログアウト</h1>
-                <button class="btn-close"></button>
+                <h2>ログアウト</h2>
+                <button class="btn-close-logout btn-close"></button>
                 <p>ログアウトしますか？</p>
-                <button>ログアウト</button>
+                <button><a class="dropdown-item" href="{{ route('logout') }}">ログアウト</a></button>
             </div>
         </li>
         <li>
-            <a href="#">メールアドレス変更</a>
-            <div>
-                <p>メールアドレス変更</p>
-                <button type="button" class="btn-close"></button>
-                <form></form>
+            <a id="eb" class="settingMenuButton">メールアドレス変更</a>
+            <div class="popupEmail">
+                <h2>メールアドレス変更</h2>
+                <label for="e-mail">Email</label>
+                <input type="text" id="e-mail" placeholder="Enter email">
+                <button class="btn-close-email btn-close"></button>
                 <button>変更</button>    
             </div>
         </li>
         <li>
-            <a href="#">パスワード変更</a>
-            <div>
-                <p>パスワード変更</p>
-                <button type="button" class="btn-close"></button>
-                <p>現在のパスワード</p>
-                <form></form>
-                <p>新しいパスワード</p>
-                <form></form>
-                <p>新しいパスワード（再入力）</p>
-                <form></form>
+            <a id="pb" class="settingMenuButton">パスワード変更</a>
+            <div class="popupPassword">
+                <h2>パスワード変更</h2>
+                <button type="button" class="btn-close-password btn-close"></button>
+                <label for="password">現在のパスワード</label>
+                <div><input type="password" id="password" placeholder="Enter password"></div>
+                <label for="password">新しいパスワード</label>
+                <div><input type="password" id="password" placeholder="Enter password"></div>
+                <label for="password">新しいパスワード（再入力）</label>
+                <div><input type="password" id="password" placeholder="Enter password"></div>
+                <button>変更</button>
             </div>
         </li>
         <li>
