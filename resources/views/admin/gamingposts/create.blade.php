b1a6f1dbbb9dd4f08b6ff54e78da1533f87bdabf
sota <mtnsot919@icloud.com>
2024-07-17 10:40:05 +0000

    更新

--- resources/views/admin/gamingposts/create.blade.php
+++ resources/views/admin/gamingposts/create.blade.php
@@ -7,42 +7,36 @@
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
 <div class="bg-02-settings">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>マリオゲームレビュー一覧新着順</h2>
-                <form action="{{ route('admin.gamingposts.create') }}" method="post" enctype="multipart/form-data">
-
-                    @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                    <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">本文</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">画像</label>
-                        <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                        </div>
-                    </div>
-                    @csrf
-                    <input type="submit" class="btn btn-primary" value="更新">
-                </form>
+    <h2>マリオゲームレビュー一覧新着順</h2>
+    <form action="{{ route('admin.gamingposts.create') }}" method="post" enctype="multipart/form-data">
+        @if (count($errors) > 0)
+            <ul>
+                @foreach($errors->all() as $e)
+                    <li>{{ $e }}</li>
+                @endforeach
+            </ul>
+        @endif
+        <div class="review-input">
+            <div class="review-score-area">
+                <label class="">評価</label>
+                <input class="rating__input hidden--visually" type="radio" id="5-star" name="rating" value="5" required />
+                <label class="rating__label" for="5-star" title="星5つ"><span class="rating__icon" aria-hidden="true"></span><span class="hidden--visually">星5つ</span></label>
+                <input class="rating__input hidden--visually" type="radio" id="4-star" name="rating" value="4" />
+                <label class="rating__label" for="4-star" title="星4つ"><span class="rating__icon" aria-hidden="true"></span><span class="hidden--visually">星4つ</span></label>
+                <input class="rating__input hidden--visually" type="radio" id="3-star" name="rating" value="3" />
+                <label class="rating__label" for="3-star" title="星3つ"><span class="rating__icon" aria-hidden="true"></span><span class="hidden--visually">星3つ/span></label>
+                <input class="rating__input hidden--visually" type="radio" id="2-star" name="rating" value="2" />
+                <label class="rating__label" for="2-star" title="星2つ"><span class="rating__icon" aria-hidden="true"></span><span class="hidden--visually">星2つ</span></label>
+                <input class="rating__input hidden--visually" type="radio" id="1-star" name="rating" value="1" />
+                <label class="rating__label" for="1-star" title="星1つ"><span class="rating__icon" aria-hidden="true"></span><span class="hidden--visually">星1つ</span></label>
+            </div>
+            <div class="review-title">
+                <input class="input-text" placeholder="レビュータイトル" type="text" name="mario_review[subject]" id="mario_review_subject">
+            </div>
+            <div class="review-txt-area">
+                <div class="review-txt"><textarea class="text-area" placeholder="本文" name="movie_review[body]" id="movie_review_body" cols="40" rows="10"></textarea></div> 
             </div>
         </div>
-    </div>
+    </form>
 </div>
 @endsection
