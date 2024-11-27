4948ff94446806ea1b43e366e3d97f6eb64be678
sota <mtnsot919@icloud.com>
2024-11-27 11:23:37 +0000

    更新

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -100,7 +100,7 @@
             <div class="d-flex">
                 <ul class="mario-nav d-flex">
                     <li><a href="{{ url('/games') }}" class="mario-nav-games">ゲームソフト</a></li>
-                    <li><a href="{{ url('/posts') }}" class="mario-nav-posts">投稿一覧</a></li>
+                    <li><a href="{{ url('/admin/gamingposts/create') }}" class="mario-nav-posts">投稿作成</a></li>
                 </ul>
                 <div><a href="#" class="up-btn"><img src="{{ secure_asset('img/up-btn.jpg') }}" alt="上へ戻るボタン"></a></div>
             </div>
--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -12,8 +12,8 @@
 @section('content')
     <div class="bg-01">
         <ul class="mario-nav d-flex justify-content-center">
-            <li><a href="{{ url('/games') }}" class="mario-nav-games">ゲームソフト</a></li>
-            <li><a href="{{ url('/posts') }}" class="mario-nav-posts">投稿一覧</a></li>
+            <li><a href="{{ url('/games') }}" class="mario-nav-posts-create">ゲームソフト</a></li>
+            <li><a href="{{ url('/posts') }}" class="mario-nav-posts-create">投稿一覧</a></li>
         </ul>
         <h1 class="letter">最新投稿</h1>
         <p class="mario-nav-posts-create"><a href="{{ url('/admin/gamingposts/create') }}">投稿作成へ</a></p>
@@ -21,9 +21,9 @@
     <div class="bg-02">
         <h2 class="letter">ゲーム一覧</h2>
         <div class="gamebox d-flex">
-            <a href="#" class="mariogame"><div><img src="{{ secure_asset('img/スーパーマリオ64.jpg') }}"></div></a>
-            <a href="#" class="mariogame"><div><img src="{{ secure_asset('img/スーパーマリオギャラクシー.jpg') }}"></div></a>
-            <a href="#" class="mariogame"><div><img src="{{ secure_asset('img/スーパーマリオオデッセイ.jpg') }}"></div></a>
+            <a href="{{ url('/admin/gamingposts/create/スーパーマリオ64') }}" class="mariogame"><div><img src="{{ secure_asset('img/スーパーマリオ64.jpg') }}"></div></a>
+            <a href="{{ url('/admin/gamingposts/create/スーパーマリオギャラクシー') }}" class="mariogame"><div><img src="{{ secure_asset('img/スーパーマリオギャラクシー.jpg') }}"></div></a>
+            <a href="{{ url('/admin/gamingposts/create/スーパーマリオオデッセイ') }}" class="mariogame"><div><img src="{{ secure_asset('img/スーパーマリオオデッセイ.jpg') }}"></div></a>
         </div>
         <p class="mario-nav-gameList"><a href="{{ url('/games') }}">もっと見る</a></p>
     </div>
