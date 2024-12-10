f106a836683524395900439bf686d5e358ba1b90
sota <mtnsot919@icloud.com>
2024-12-10 07:39:48 +0000

    更新

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -63,24 +63,24 @@
                             </div>
                         </li>
                     @endguest
-                    <li class="hamburger">
-                        <!-- ハンバーガーメニュー部分 -->
-                        <div class="nav">
-                            <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
-                            <input id="drawer_input" class="drawer_hidden" type="checkbox">
-                            <!-- ハンバーガーアイコン -->
-                            <label for="drawer_input" class="drawer_open"><span></span></label>
-                            <!-- メニュー -->
-                            <nav class="nav_content">
-                                <ul class="nav_list">
-                                    <li class="nav_item"><a href="">トップページ</a></li>
-                                    <li class="nav_item"><a href="">ゲーム一覧</a></li>
-                                    <li class="nav_item"><a href="">投稿一覧</a></li>
-                                    <li class="nav_item"><a href="">アカウント設定</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </li>
+                    <!--<li class="hamburger">-->
+                    <!--     ハンバーガーメニュー部分 -->
+                    <!--    <div class="nav">-->
+                    <!--         ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+                    <!--        <input id="drawer_input" class="drawer_hidden" type="checkbox">-->
+                    <!--         ハンバーガーアイコン -->
+                    <!--        <label for="drawer_input" class="drawer_open"><span></span></label>-->
+                    <!--         メニュー -->
+                    <!--        <nav class="nav_content">-->
+                    <!--            <ul class="nav_list">-->
+                    <!--                <li class="nav_item"><a href="">トップページ</a></li>-->
+                    <!--                <li class="nav_item"><a href="">ゲーム一覧</a></li>-->
+                    <!--                <li class="nav_item"><a href="">投稿一覧</a></li>-->
+                    <!--                <li class="nav_item"><a href="">アカウント設定</a></li>-->
+                    <!--            </ul>-->
+                    <!--        </nav>-->
+                    <!--    </div>-->
+                    <!--</li>-->
                 </ul>
                 <div class="logout-box">
                     <h4>ログアウト</h4>
